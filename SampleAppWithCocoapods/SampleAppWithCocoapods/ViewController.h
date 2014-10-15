5bf6e4532746b7453abb3ad66886436de3391825
Viktor Benei <viktor.benei@gmail.com>
2014-10-15 11:42:58 +0200

    base app project

---
+++ SampleAppWithCocoapods/SampleAppWithCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleAppWithCocoapods
+//
+//  Created by Viktor Benei on 10/15/14.
+//  Copyright (c) 2014 Bitrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleAppWithCocoapods/SampleAppWithCocoapods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleAppWithCocoapods
+//
+//  Created by Viktor Benei on 10/15/14.
+//  Copyright (c) 2014 Bitrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
